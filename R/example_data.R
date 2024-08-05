be137c919771f71998a19810ed29339c1fbf1afe
Kalash Singhal <kalash@2degrees-investing.org>
2024-08-05 11:26:33 +0200

    update example data to increase coverage for all benchmarks

--- R/example_data.R
+++ R/example_data.R
@@ -12,15 +12,15 @@
 example_emission_product_co2_des_analysis <- function() {
   tribble(
   # styler: off
-          ~benchmark, ~tilt_subsector, ~co2_footprint,   ~ep_product,
-    "tilt_subsector",     "chemicals",              1,       "water",
-    "tilt_subsector",     "chemicals",              4,  "television",
-    "tilt_subsector",     "chemicals",              9,      "tables",
-    "tilt_subsector",     "chemicals",              8, "electricity",
-    "tilt_subsector",     "chemicals",       NA_real_,        "meat",
-    "tilt_subsector",  "iron & steel",              1,      "tables",
-    "tilt_subsector",  "iron & steel",              2,       "water",
-    "tilt_subsector",  "iron & steel",              4,        "tent"
+          ~benchmark, ~tilt_subsector, ~unit, ~isic_4digit, ~co2_footprint,   ~ep_product,
+    "tilt_subsector",     "chemicals",  "kg",     "'5555'",              1,       "water",
+    "tilt_subsector",     "chemicals",  "kg",     "'5555'",              4,  "television",
+    "tilt_subsector",     "chemicals",  "kg",     "'5555'",              9,      "tables",
+    "tilt_subsector",     "chemicals",  "kg",     "'5555'",              8, "electricity",
+    "tilt_subsector",     "chemicals",  "kg",     "'5555'",       NA_real_,        "meat",
+    "tilt_subsector",  "iron & steel",  "m3",     "'6666'",              1,      "tables",
+    "tilt_subsector",  "iron & steel",  "m3",     "'6666'",              2,       "water",
+    "tilt_subsector",  "iron & steel",  "m3",     "'6666'",              4,        "tent"
     # styler: on
   )
 }
