db9897e9cfb6f69ecd350ce35e050204dd512d66
Kalash Singhal <kalash@2degrees-investing.org>
2024-09-02 13:13:37 +0200

    Update methodology for co2 footprint per benchmark

--- R/calculate_co2_descriptive_analysis_per_benchmark.R
+++ R/calculate_co2_descriptive_analysis_per_benchmark.R
@@ -19,32 +19,32 @@
 #'   emission_product_example,
 #'   "tilt_subsector"
 #' )
-calculate_co2_descriptive_analysis_per_benchmark <- function(emission_product, benchmark) {
-  if (benchmark == "all") {
-    select_benchmarks <- c("benchmark", "co2_footprint", "ep_product")
+calculate_co2_descriptive_analysis_per_benchmark <- function(emission_product, grouping_emission) {
+  if (grouping_emission == "all") {
+    select_benchmarks <- c("grouping_emission", "co2_footprint", "activity_uuid_product_uuid")
     group_benchmark <- NULL
-  } else if (benchmark == "unit_isic_4digit") {
-    select_benchmarks <- c("unit", "isic_4digit", "benchmark", "co2_footprint", "ep_product")
+  } else if (grouping_emission == "unit_isic_4digit") {
+    select_benchmarks <- c("unit", "isic_4digit", "grouping_emission", "co2_footprint", "activity_uuid_product_uuid")
     group_benchmark <- c("unit", "isic_4digit")
-  } else if (benchmark == "unit_tilt_subsector") {
-    select_benchmarks <- c("tilt_sector", "unit", "tilt_subsector", "benchmark", "co2_footprint", "ep_product")
+  } else if (grouping_emission == "unit_tilt_subsector") {
+    select_benchmarks <- c("tilt_sector", "unit", "tilt_subsector", "grouping_emission", "co2_footprint", "activity_uuid_product_uuid")
     group_benchmark <- c("unit", "tilt_subsector")
   } else {
-    select_benchmarks <- c("tilt_sector", benchmark, "benchmark", "co2_footprint", "ep_product")
-    group_benchmark <- benchmark
+    select_benchmarks <- c("tilt_sector", grouping_emission, "grouping_emission", "co2_footprint", "activity_uuid_product_uuid")
+    group_benchmark <- grouping_emission
   }
 
   output <- emission_product |>
     select(all_of(select_benchmarks)) |>
     distinct() |>
-    filter(benchmark == benchmark) |>
-    select(-c("benchmark")) |>
-    mutate("# of distinct products" = n_distinct(.data$ep_product, na.rm = TRUE), .by = group_benchmark) |>
+    filter(grouping_emission == grouping_emission) |>
+    select(-c("grouping_emission")) |>
+    mutate("# of distinct products" = n_distinct(.data$activity_uuid_product_uuid, na.rm = TRUE), .by = group_benchmark) |>
     mutate("min CO2e" = min(.data$co2_footprint, na.rm = TRUE), .by = group_benchmark) |>
     mutate("max CO2e" = max(.data$co2_footprint, na.rm = TRUE), .by = group_benchmark) |>
     mutate("mean CO2e" = mean(.data$co2_footprint, na.rm = TRUE), .by = group_benchmark) |>
     mutate("median CO2e" = median(.data$co2_footprint, na.rm = TRUE), .by = group_benchmark) |>
-    select(-all_of(c("ep_product", "co2_footprint"))) |>
+    select(-all_of(c("activity_uuid_product_uuid", "co2_footprint"))) |>
     distinct()
 
   output[order(output[[1]], output[[2]]), ]
--- R/example_data.R
+++ R/example_data.R
@@ -24,15 +24,15 @@
 example_emission_product_co2_des_analysis <- function() {
   tribble(
   # styler: off
-          ~benchmark, ~tilt_sector, ~tilt_subsector, ~unit, ~isic_4digit, ~co2_footprint,   ~ep_product,
-    "tilt_subsector",      "group",     "chemicals",  "kg",     "'5555'",              1,       "water",
-    "tilt_subsector",      "group",     "chemicals",  "kg",     "'5555'",              4,  "television",
-    "tilt_subsector",      "group",     "chemicals",  "kg",     "'5555'",              9,      "tables",
-    "tilt_subsector",      "group",     "chemicals",  "kg",     "'5555'",              8, "electricity",
-    "tilt_subsector",      "group",     "chemicals",  "kg",     "'5555'",       NA_real_,        "meat",
-    "tilt_subsector",      "group",  "iron & steel",  "m3",     "'6666'",              1,      "tables",
-    "tilt_subsector",      "group",  "iron & steel",  "m3",     "'6666'",              2,       "water",
-    "tilt_subsector",      "group",  "iron & steel",  "m3",     "'6666'",              4,        "tent"
+  ~grouping_emission, ~tilt_sector, ~tilt_subsector, ~unit, ~isic_4digit, ~co2_footprint,      ~product, ~activity_uuid_product_uuid,
+    "tilt_subsector",      "group",     "chemicals",  "kg",     "'5555'",              1,       "water",                     "uuid1",
+    "tilt_subsector",      "group",     "chemicals",  "kg",     "'5555'",              4,  "television",                     "uuid2",
+    "tilt_subsector",      "group",     "chemicals",  "kg",     "'5555'",              9,      "tables",                     "uuid3",
+    "tilt_subsector",      "group",     "chemicals",  "kg",     "'5555'",              8, "electricity",                     "uuid4",
+    "tilt_subsector",      "group",     "chemicals",  "kg",     "'5555'",       NA_real_,        "meat",                     "uuid5",
+    "tilt_subsector",      "group",  "iron & steel",  "m3",     "'6666'",              1,      "tables",                     "uuid3",
+    "tilt_subsector",      "group",  "iron & steel",  "m3",     "'6666'",              2,       "water",                     "uuid1",
+    "tilt_subsector",      "group",  "iron & steel",  "m3",     "'6666'",              4,        "tent",                     "uuid6"
     # styler: on
   )
 }
