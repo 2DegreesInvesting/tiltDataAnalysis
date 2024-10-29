8f39057a6b43ea98be966b9898bd9ac126e5926e
Kalash Singhal <125359076+kalashsinghal@users.noreply.github.com>
2024-10-29 17:51:47 +0530

    Add descriptive analysis of best case worst case for sector profile at product level (#49)
    * refactor
    
    * refactor

--- R/tiltDataAnalysis-package.R
+++ R/tiltDataAnalysis-package.R
@@ -22,7 +22,6 @@
 #' @importFrom tiltIndicator unnest_product
 #' @importFrom tiltIndicatorAfter profile_emissions
 #' @importFrom tiltIndicatorAfter profile_sector
-#' @importFrom tiltIndicatorAfter transition_risk_profile
 #' @importFrom tiltToyData toy_ecoinvent_activities
 #' @importFrom tiltToyData toy_ecoinvent_europages
 #' @importFrom tiltToyData toy_ecoinvent_inputs
@@ -33,6 +32,7 @@
 #' @importFrom tiltToyData toy_sector_profile_any_scenarios
 #' @importFrom tiltToyData toy_sector_profile_companies
 #' @importFrom tiltTransitionRisk toy_all_activities_scenario_sectors
+#' @importFrom tiltTransitionRisk transition_risk_profile
 #' @importFrom utils data
 ## usethis namespace: end
 NULL
